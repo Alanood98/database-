08d11c69669ba5cf006cdc5c6fc44587743d2588
Alanoud <130397461+Alanood98@users.noreply.github.com>
2024-10-20 16:46:14 +0400

    Add files via upload

---
+++ SQLQuery1, lab1.sql
@@ -0,0 +1,40 @@
+
+use lab1
+
+create Table Instructor 
+(
+	Id int primary key identity(1,1 ),
+	firstN nvarchar(20)NOT NULL,
+	LastN nvarchar(20) NOT NULL,
+	BD date,
+	Salary int  CONSTRAINT S_Salary check(Salary Between 1000 and 5000) default 3000, 
+	overTime int unique NOT NULL,
+	NetSalary as (Salary+OverTime),
+	Age as (year(getdate())-year(BD)) ,
+	hiredate date default getdate(),
+	aaddress nvarchar(50) CHECK (aaddress IN ('cairo ', 'alex ')),
+	
+)
+create Table Course 
+(
+    CId int primary key identity(1,1 ),
+	Cname nvarchar(10) NOT NULL,
+	Duration int unique NOT NULL
+)
+
+create Table Lab 
+(
+    LId int primary key identity(1,1 ),
+	Location nvarchar(50) NOT NULL,
+	Capacity int check (capacity <20),
+	CId int NOT NULL,
+	foreign key (CId) references Course(CId)
+)
+
+create Table Instructor_Course 
+(   Id int NOT NULL,
+    CId int NOT NULL,
+	Foreign key (Id) references Instructor(Id),
+	Foreign key (CId) references Course(CId),
+	primary key (Id, CId)
+)
